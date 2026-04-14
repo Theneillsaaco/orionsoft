f44f921de0c7723dfe64379f07a7c02b310f08bf
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-14 19:14:49 -0400

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,14 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Theneillsaaco.github.io',
+  base: '/orionsoft',
+  
+  server:{
+    host: true,
+    allowedHosts: true
+  },
+
   vite: {
     plugins: [tailwindcss()]
   },
